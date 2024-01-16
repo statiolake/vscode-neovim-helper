fc2c2a2c64720c0bed809787cec4bdf4d3793296
statiolake <statiolake@gmail.com>
2024-01-17 00:57:24 +0900

    feat: wip: try all possible strategy to merge editor...

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,13 @@
 import * as vscode from "vscode";
 
+function tabLabelInputToString(a?: vscode.Tab): string {
+  return `[${a?.label}, ${JSON.stringify(a?.input)}]`;
+}
+
+function isEqualTabLabel(a: vscode.Tab, b: vscode.Tab): boolean {
+  return tabLabelInputToString(a) === tabLabelInputToString(b);
+}
+
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand("neovim-helper.showTabGroups", () => {
@@ -12,36 +20,95 @@ export function activate(context: vscode.ExtensionContext) {
       });
     }),
     vscode.commands.registerCommand(
+      "neovim-helper.showEditorLayout",
+      async () => {
+        const layout = await vscode.commands.executeCommand(
+          "vscode.getEditorLayout"
+        );
+        console.log(`Current editor layout: ${JSON.stringify(layout)}`);
+      }
+    ),
+    vscode.commands.registerCommand(
       "neovim-helper.mergeCurrentEditorGroupToSomewhere",
       async () => {
-        // workbench.action.focusPreviousGroup
-        // workbench.action.focusNextGroup
-        // workbench.action.moveEditorToPreviousGroup
-        const currentTabGroup = vscode.window.tabGroups.activeTabGroup;
-        await vscode.commands.executeCommand(
-          "workbench.action.focusPreviousGroup"
+        const layout = await vscode.commands.executeCommand(
+          "vscode.getEditorLayout"
         );
-        if (currentTabGroup === vscode.window.tabGroups.activeTabGroup) {
-          console.log("There is only one tabgroup.");
-          return;
-        }
-
-        const previousTabGroup = vscode.window.tabGroups.activeTabGroup;
-        const activeTabInPreviousTabGroup = previousTabGroup.activeTab;
-        await vscode.commands.executeCommand("workbench.action.focusNextGroup");
-
-        while (vscode.window.tabGroups.activeTabGroup === currentTabGroup) {
-          await vscode.commands.executeCommand(
-            "workbench.action.moveEditorToPreviousGroup"
-          );
-        }
-
-        while (
-          vscode.window.tabGroups.activeTabGroup.activeTab?.label !==
-          activeTabInPreviousTabGroup?.label
-        ) {
-          await vscode.commands.executeCommand("workbench.action.nextEditor");
-        }
+        const currentViewColumn =
+          vscode.window.tabGroups.activeTabGroup.viewColumn;
+        // // workbench.action.focusPreviousGroup
+        // // workbench.action.focusNextGroup
+        // // workbench.action.moveEditorToPreviousGroup
+        // // workbench.action.joinTwoGroups [GroupDirection.RIGHT, GroupDirection.DOWN, GroupDirection.LEFT, GroupDirection.UP]
+        // // Try accessing editor group that current editor group will be merged
+        // // into. To restore active tab in that group, we need to record the
+        // // active tab in that tab group first.
+        // const initialViewColumn =
+        //   vscode.window.tabGroups.activeTabGroup.viewColumn;
+        // console.log(`initial view column: ${initialViewColumn}`);
+        // let initialActiveTab: vscode.Tab | undefined;
+        // let foundEditorGroup = false;
+        // const findingDirections = ["Right", "Below", "Left", "Above"];
+        // const findingOppositeDirections = ["Left", "Above", "Right", "Below"];
+        // for (let i = 0; i < 4; i++) {
+        //   const dir = findingDirections[i];
+        //   const oppositeDir = findingOppositeDirections[i];
+        //   const command = `workbench.action.focus${dir}Group`;
+        //   await vscode.commands.executeCommand(command);
+        //   if (
+        //     initialViewColumn !==
+        //     vscode.window.tabGroups.activeTabGroup.viewColumn
+        //   ) {
+        //     console.log(
+        //       `will be merged to ${dir}: ${vscode.window.tabGroups.activeTabGroup.viewColumn}`
+        //     );
+        //     // Succeeded to move. Record the active tab.
+        //     initialActiveTab = vscode.window.tabGroups.activeTabGroup.activeTab;
+        //     console.log(
+        //       `initial active editor: ${tabLabelInputToString(
+        //         initialActiveTab
+        //       )}`
+        //     );
+        //     const command = `workbench.action.focus${oppositeDir}Group`;
+        //     await vscode.commands.executeCommand(command);
+        //     console.log(
+        //       `focus back to ${dir}: ${vscode.window.tabGroups.activeTabGroup.viewColumn}`
+        //     );
+        //     foundEditorGroup = true;
+        //     break;
+        //   }
+        // }
+        // if (!foundEditorGroup) {
+        //   console.log("it seems current view is single one");
+        //   return;
+        // }
+        // await vscode.commands.executeCommand("workbench.action.joinTwoGroups");
+        // // Restore original active tab
+        // const maxSwitch = vscode.window.tabGroups.all
+        //   .map((tabGroup) => tabGroup.tabs.length)
+        //   .reduce((sum, numTabs) => sum + numTabs);
+        // for (let i = 0; i < maxSwitch; i++) {
+        //   const activeTab = vscode.window.tabGroups.activeTabGroup.activeTab;
+        //   console.log(
+        //     `active tab: (${activeTab?.label}, ${JSON.stringify(
+        //       activeTab?.input
+        //     )})`
+        //   );
+        //   if (
+        //     tabLabelInputToString(initialActiveTab) ===
+        //     tabLabelInputToString(activeTab)
+        //   ) {
+        //     console.log(`-> found tab`);
+        //     break;
+        //   }
+        //   await vscode.commands.executeCommand("workbench.action.nextEditor");
+        // }
+        // const activeTab = vscode.window.tabGroups.activeTabGroup.activeTab;
+        // console.log(
+        //   `restore complete: (${activeTab?.label}, ${JSON.stringify(
+        //     activeTab?.input
+        //   )})`
+        // );
       }
     )
   );
